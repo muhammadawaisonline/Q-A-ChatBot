27c026dc8721329bfb7e4aa98765dd6f74586d1d
muhammad awais online <muhammadawaisonline00@gmail.com>
2024-11-03 17:15:46 -0500

    RAG QA Chatbot

--- 1.3 RAG QA Chatbot/app.py
+++ 1.3 RAG QA Chatbot/app.py
@@ -40,9 +40,40 @@ prompt = ChatPromptTemplate.from_template(
 
 
 def create_vector_embedding():
-    if "vector" not in st.session_state:
+    if "vectors" not in st.session_state:
         st.session_state.embeddings = OllamaEmbeddings()
         st.session_state.loader = PyPDFDirectoryLoader("research_paper")
+        st.session_state.docs = st.session_state.loader.load()
+        st.session_state.text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
+        st.session_state.final_documents = st.session_state.text_splitter.split_documents(st.session_state.docs[:50])
+        st.session_state.vectors = FAISS.from_documents(st.session_state.final_documents, st.session_state.embeddings)
+
+
+user_prompt = st.text_input("Enter your query from the research paper")
+
+if st.button("Documents Embeddings"):
+    create_vector_embedding()
+    st.write("Vector Database is ready")
+
+import time
+
+if user_prompt:
+    document_chain = create_stuff_documents_chain(llm, prompt)
+    retriever = st.session_state.vectors.as_retriever()
+    retrieval_chain = create_retrieval_chain(retriever, document_chain)
+
+    start = time.process_time()
+    response = retrieval_chain.invoke({"input": user_prompt})
+    print(f"Response Time: {time.process_time()- start}")
+    st.write(response["answer"])
+
+
+    ## With a Stream Expender 
+    with st.expander("Document Similarity Search"):
+        for i, docs in enumerate(response["context"]):
+            st.write(docs.page_content)
+            st.write("--------------")
+
 
 
 
