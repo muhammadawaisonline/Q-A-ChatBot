a2604cd55ff6a3719325e03c47dfcf1fa489549e
muhammad awais online <muhammadawaisonline00@gmail.com>
2024-11-04 18:05:45 -0500

    RAG Libararies

--- 1.4 Conversational App/app.py
+++ 1.4 Conversational App/app.py
@@ -1,3 +1,8 @@
 import streamlit as st
 from langchain.chains.retrieval import create_retrieval_chain
-from langchain.chains import 
+from langchain.chains.history_aware_retriever import create_history_aware_retriever
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_chroma import Chroma
+import os
+from dotenv import load_dotenv
+load_dotenv()
