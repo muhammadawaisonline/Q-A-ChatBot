6d1bd7548fda83b646d1de5065c1eaa54068a141
muhammad awais online <muhammadawaisonline00@gmail.com>
2024-11-05 19:07:32 -0500

    RAG Update

--- 1.4 Conversational App/app.py
+++ 1.4 Conversational App/app.py
@@ -6,3 +6,13 @@ from langchain_chroma import Chroma
 import os
 from dotenv import load_dotenv
 load_dotenv()
+from langchain_community.chat_message_histories import ChatMessageHistory
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_groq import ChatGroq
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.document_loaders import PyPDFLoader
+
+os.environ["HUGGINGFACE_TOKEN"] = os.getenv("HUGGINGFACE_TOKEN")
+embeddings = HuggingFaceEmbeddings(model_name= "all-MiniLM-L6-v2")
+
