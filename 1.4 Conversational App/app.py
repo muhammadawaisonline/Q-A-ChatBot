c8d991bde326dd4fcb3bbead1985c9954620e4b4
muhammad awais online <muhammadawaisonline00@gmail.com>
2024-11-06 15:29:07 -0500

    Uploading PDF in your App

--- 1.4 Conversational App/app.py
+++ 1.4 Conversational App/app.py
@@ -12,7 +12,42 @@ from langchain_groq import ChatGroq
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.document_loaders import PyPDFLoader
+from langchain_core.runnables.history import RunnableWithMessageHistory
 
 os.environ["HUGGINGFACE_TOKEN"] = os.getenv("HUGGINGFACE_TOKEN")
 embeddings = HuggingFaceEmbeddings(model_name= "all-MiniLM-L6-v2")
+
+#Setup Streamlit App
+st.title("Rag App with Converation Chat History with PDF")
+st.write("Ipload PDFs and Chat with their content")
+
+##Input the Qroq API Key
+api_key = st.text_input("Enter Your Qroq API Key:", type= "password")
+
+## Check if Qroq Api key is provided.
+if api_key:
+    llm = ChatGroq(api_key=api_key, model="Gamma2-9b-it")
+
+
+
+## Chat Interface
+    session_id = st.text_input("Session ID", value= "default_session")
+
+    if "store" not in st.session_state:
+        st.session_state.store = {}
+    upload_files = st.file_uploader("Choose a pdf files", type= "pdf", accept_multiple_files=True)
+
+    if upload_files:
+        documents = []
+        for upload_file in upload_files:
+            temppdf = f"./temp.pdf"
+            with open(temppdf, "wb") as file:
+                file.write(upload_file.getvalue())
+                file_name = upload_file.name
+        loader = PyPDFLoader(temppdf)
+        docs = loader.load()
+        documents.extend(docs)
+        
+         
+
 
