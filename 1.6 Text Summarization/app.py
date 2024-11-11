cae971f9f9984a425858ac89304ed3131111184c
muhammad awais online <muhammadawaisonline00@gmail.com>
2024-11-11 15:28:03 -0500

    Summarizing YT Video or website page content

--- 1.6 Text Summarization/app.py
+++ 1.6 Text Summarization/app.py
@@ -45,5 +45,13 @@ if st.button("Summarize the content from YT or website"):
                                                    headers= {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36"})
                 docs= loader.load()
 
-               
+                ## Chain for summarization 
+                chain = load_summarize_chain(llm=llm, 
+                                             chain_type="stuff",
+                                             prompt= prompt)
+                output_summary = chain.run(docs)
+
+                st.success(output_summary)
+        except Exception as e:
+            st.exception(f"Exceptions: {e}")
             
