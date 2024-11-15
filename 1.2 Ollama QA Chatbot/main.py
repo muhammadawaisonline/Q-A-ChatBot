6e1b772587fd28f83e2456c83ecf2d564402b0b1
muhammad awais online <muhammadawaisonline00@gmail.com>
2024-11-14 22:02:19 -0500

    Prompt Template

--- 1.2 Ollama QA Chatbot/main.py
+++ 1.2 Ollama QA Chatbot/main.py
@@ -13,3 +13,21 @@ os.environ["LANGCHAIN_API_KEY"] = os.getenv("LANGCHAIN_API_KEY")
 os.environ["LANGCHAIN_TRACING_V2"] = "true"
 os.environ["LANGCHAIN_PROJECT"] = "QA Chatbot"
 
+# defining chat prompt
+
+prompt =  ChatPromptTemplate.from_messages(
+
+    [
+        ("system", "You are helpful asistant. Please respond to the queries."),
+        ("user", "Question: {question}")
+
+    ]
+)
+
+def generate_response(question, engine, temprature, max_tokens):
+    llm = ollama(model= engine)
+    output_parser = StrOutputParser()
+    chain= prompt | llm | output_parser
+    answer = chain.invoke({"question": question})
+    return answer
+
